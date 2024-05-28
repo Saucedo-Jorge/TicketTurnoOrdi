f99ae340daa5066dc2b81347cfc5759efbe63249
Jorge Saucedo <saucedojorge@uadec.edu.mx>
2024-05-28 16:24:15 -0600

    eliminacion de segunda opcion ordi

--- SegundaOpcion/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')
-    DB_USER = os.getenv('DB_USER', 'root')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', '1234')
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_NAME = os.getenv('DB_NAME', 'ticket')
